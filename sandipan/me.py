576975172e06805914dbe07cb19002b2d41674b4
Sandipan Roy <sandipan@parrotsec.org>
2020-10-04 13:09:06 +0530

    update-0.0.8

--- sandipan/me.py
+++ sandipan/me.py
@@ -2,14 +2,18 @@
 
 
 def personal_details():
-    name, age = "Sandipan Roy", 20
+    name, age = "Sandipan Roy", 23
     address = "Berhampore, West Bengal, India"
     print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
 def social_links():
     twitter = "twitter.com/bytehackr"
     linkedin = "linkedin/in/bytehackr"
-    email = "me@sandipan.ml"
+    instagram = "instagram.com/bytehackr"
+    email = "'sandipan@parrotsec.org' or 'me@sandipan.ml'"
     print("Email: {}\nTwitter: {}\nLinkedin: {}".format(email, twitter, linkedin))
 
+
+print("-----------------")
 personal_details()
+print("-----------------")
 social_links()
